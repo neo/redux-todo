c5e67bf45c3d4f09981cab907b64ba2456bfecf4
Neo <neo.li@jam3.com>
2017-05-05 15:35:50 -0400

    Add component Header

---
+++ src/components/Header.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { addTodo } from '../store/actions';
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addTodo: id => dispatch(addTodo(id))
+  }
+};
+
+class Header extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    const input = e.target.querySelector('input');
+    const value = input.value.trim();
+    if (value.length > 0) this.props.addTodo(value);
+    input.value = '';
+  };
+
+  render() {
+    return (
+      <header className="header">
+        <h1>todos</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            className="new-todo"
+            placeholder="What needs to be done?"
+            autoFocus={true}
+          />
+        </form>
+      </header>
+    );
+  }
+}
+
+Header.propTypes = {
+  addTodo: PropTypes.func.isRequired
+};
+
+Header.defaultProps = {
+  addTodo: () => {}
+};
+
+export default connect(null, mapDispatchToProps)(Header);
--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ import { createStore } from 'redux';
 import todoApp from './store/reducers';
 import App from './components/App';
 
-let store = createStore(todoApp);
+let store = createStore(
+  todoApp,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
