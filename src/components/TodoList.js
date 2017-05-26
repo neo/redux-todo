23f584db6262512b61653f15446ab3c60c8eb664
Neo <neo.li@jam3.com>
2017-05-26 15:40:50 -0400

    Implement visibilityFilter into TodoList

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import { toggleTodo } from '../store/actions';
+import { VisibilityFilters } from '../store/ActionTypes';
 
 import TodoItem from './TodoItem';
 
 const mapStateToProps = state => {
   return {
-    todos: state.todos
+    todos: state.todos,
+    visibilityFilter: state.visibilityFilter
   };
 };
 
@@ -20,17 +22,31 @@ const mapDispatchToProps = dispatch => {
 
 class TodoList extends Component {
   render() {
+    if (this.props.todos.ids.length === 0) {
+      return null;
+    }
+
     return (
       <section className="main">
         <input type="checkbox"
           className="toggle-all"/>
         <label htmlFor="toggle-all">Mark all as complete</label>
         <ul className="todo-list">
-          {this.props.todos.ids.map(id => <TodoItem
-            key={id} id={id}
-            onToggle={this.props.toggleTodo}
-            {...this.props.todos.todosById[id]}
-          />)}
+          {
+            this.props.todos.ids
+              .filter(id => {
+                const completed = this.props.todos.todosById[id].completed;
+                return !(
+                  (this.props.visibilityFilter === VisibilityFilters.SHOW_ACTIVE && completed) ||
+                  (this.props.visibilityFilter === VisibilityFilters.SHOW_COMPLETED && !completed)
+                );
+              })
+              .map(id => <TodoItem
+                key={id} id={id}
+                onToggle={this.props.toggleTodo}
+                {...this.props.todos.todosById[id]}
+              />)
+          }
         </ul>
       </section>
     );
@@ -42,6 +58,7 @@ TodoList.propTypes = {
     ids: PropTypes.arrayOf(PropTypes.number),
     todosById: PropTypes.object.isRequired
   }),
+  visibilityFilter: PropTypes.string.isRequired,
   toggleTodo: PropTypes.func.isRequired
 };
 
